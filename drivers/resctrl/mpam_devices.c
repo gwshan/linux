811dd81a25ed1cb2abb6c79bd27035e0946bd659
James Morse <james.morse@arm.com>
2019-03-04 15:15:25 +0000

    arm_mpam: resctrl: Implement resctrl_arch_reset_all_ctrls()
    We already have a helper for resetting an mpam class and component.
    Hook it up to resctrl_arch_reset_all_ctrls() and the domain offline
    path.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- drivers/resctrl/mpam_devices.c
+++ drivers/resctrl/mpam_devices.c
@@ -2683,7 +2683,7 @@ static void mpam_enable_once(void)
 	       mpam_partid_max + 1, mpam_pmg_max + 1);
 }
 
-static void mpam_reset_component_locked(struct mpam_component *comp)
+void mpam_reset_component_locked(struct mpam_component *comp)
 {
 	int idx;
 	struct mpam_msc *msc;
@@ -2709,7 +2709,7 @@ static void mpam_reset_component_locked(struct mpam_component *comp)
 	srcu_read_unlock(&mpam_srcu, idx);
 }
 
-static void mpam_reset_class_locked(struct mpam_class *class)
+void mpam_reset_class_locked(struct mpam_class *class)
 {
 	int idx;
 	struct mpam_component *comp;
@@ -2722,7 +2722,7 @@ static void mpam_reset_class_locked(struct mpam_class *class)
 	srcu_read_unlock(&mpam_srcu, idx);
 }
 
-static void mpam_reset_class(struct mpam_class *class)
+void mpam_reset_class(struct mpam_class *class)
 {
 	cpus_read_lock();
 	mpam_reset_class_locked(class);
