c698f122aafddba872d3b2fb242d9efb86be5962
Steven Price <steven.price@arm.com>
2021-10-13 16:11:19 +0100

    KVM: arm64: CCA: Tear down RTTs
    The RMM owns the stage 2 page tables for a realm, and KVM must request
    that the RMM creates/destroys entries as necessary. The physical pages
    to store the page tables are delegated to the realm as required, and can
    be undelegated when no longer used.
    
    Creating new RTTs is the easy part, tearing down is a little more
    tricky. The result of realm_rtt_destroy() can be used to effectively
    walk the tree and destroy the entries (undelegating pages that were
    given to the realm).
    
    Unmap the Realm IPA space once during stage-2 teardown. Defer destruction
    of non-root RTTs until final Realm destruction, after REALM_TERMINATE and
    before destroying the RD; root RTTs remain until the RD has gone away.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

--- arch/arm64/include/asm/kvm_rmi.h
+++ arch/arm64/include/asm/kvm_rmi.h
@@ -57,6 +57,7 @@ enum realm_state {
  * @sro: Preallocated SRO state context for Realm MMU operations
  * @state: The lifetime state machine for the realm
  * @ia_bits: Number of valid Input Address bits in the IPA
+ * @stage2_unmapped: The Realm stage-2 mappings have been removed
  */
 struct realm {
 	void *rd;
@@ -70,6 +71,7 @@ struct realm {
 	struct rmi_sro_state *sro;
 	enum realm_state state;
 	unsigned int ia_bits;
+	bool stage2_unmapped;
 };
 
 void kvm_init_rmi(void);
@@ -77,6 +79,13 @@ u32 kvm_rmm_ipa_limit(void);
 
 int kvm_init_realm(struct kvm *kvm);
 void kvm_destroy_realm(struct kvm *kvm);
+int kvm_realm_teardown_stage2(struct kvm *kvm);
+
+static inline bool kvm_realm_is_private_address(struct realm *realm,
+						unsigned long addr)
+{
+	return !(addr & BIT(realm->ia_bits - 1));
+}
 
 static inline bool kvm_realm_ext_allowed(long ext)
 {
