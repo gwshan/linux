8d66d8446e6ada8a6b87e230a1d7bca8109b7c6c
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-01-04 17:46:05 +0000

    KVM: arm64: CCA: Control user register access for Realms
    The RMM restricts the access to the register states that the host can
    read/modify for a given Realm.
    
    e.g., At VCPU creation, can modify GPRS (x0-x30) and PC.
          While servicing SMCCC calls via RSI_HOST_CALL or servicing PSCI
          requests.
          MMIO emulation in the unprotected space.
    
    Additionally we use the sysreg configuration to advertise/configure the
    following Realm parameters, which are required before the Realm Descriptor
    :w
    is created:
     - SVE Vector Length
     - Number of HW Breakpoints/Watchpoints
     - PMU Counters.
    
    Thus KVM also additionally allows access to ID_AA64DFR0_EL1 and SVE_VLS for
    the configuration of Realm creation parameters. We don't support PMUs for
    the Realm VMs yet, so PMCR is not exposed.
    
    The RMM makes similar restrictions for reading of the guest's registers
    (this is *confidential* compute after all), however we don't impose the
    restriction here. This allows the VMM to read (stale) values from the
    registers which might be useful to read back the initial values even if
    the RMM doesn't provide the latest version. For migration of a realm VM,
    a new interface will be needed so that the VMM can receive an
    (encrypted) blob of the VM's state.
    
    Reflect the above in KVM_GET_REG_LIST, KVM_SET_ONE_REG calls.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Co-developed-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    Co-developed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    ---
    Changes since v18:
     - Don't expose PMCR_EL0 to the userspace now, we could do that when we
       support PMU
     - Fix check patch warnings
    Changes since v17:
      - Merge related changes into one single patch for the user set/get registers
        I have retained the Review tags, as the code hasn't changed, just the patches
        were merged into a single one with the same tags.
      - Limit KVM_GET_REG_LIST to the allowed CORE registers.

--- arch/arm64/kvm/guest.c
+++ arch/arm64/kvm/guest.c
@@ -73,6 +73,25 @@ static u64 core_reg_offset_from_id(u64 id)
 	return id & ~(KVM_REG_ARCH_MASK | KVM_REG_SIZE_MASK | KVM_REG_ARM_CORE);
 }
 
+static bool kvm_realm_validate_core_reg(u64 off)
+{
+	/*
+	 * Note that GPRs can only sometimes be controlled by the VMM.
+	 * For PSCI only X0-X6 are used, higher registers are ignored (restored
+	 * from the REC).
+	 * For HOST_CALL all of X0-X30 are copied to the RsiHostCall structure.
+	 * For emulated MMIO X0 is always used.
+	 * PC can only be set before the realm is activated.
+	 */
+	switch (off) {
+	case KVM_REG_ARM_CORE_REG(regs.regs[0]) ...
+	     KVM_REG_ARM_CORE_REG(regs.regs[30]):
+	case KVM_REG_ARM_CORE_REG(regs.pc):
+		return true;
+	}
+	return false;
+}
+
 static int core_reg_size_from_offset(const struct kvm_vcpu *vcpu, u64 off)
 {
 	int size;
@@ -553,6 +572,9 @@ static int copy_core_reg_indices(const struct kvm_vcpu *vcpu,
 		u64 reg = KVM_REG_ARM64 | KVM_REG_ARM_CORE | i;
 		int size = core_reg_size_from_offset(vcpu, i);
 
+		if (vcpu_is_rec(vcpu) && !kvm_realm_validate_core_reg(i))
+			continue;
+
 		if (size < 0)
 			continue;
 
@@ -598,6 +620,9 @@ static unsigned long num_sve_regs(const struct kvm_vcpu *vcpu)
 	if (!vcpu_has_sve(vcpu))
 		return 0;
 
+	if (kvm_vm_is_realm(vcpu->kvm))
+		return 1; /* KVM_REG_ARM64_SVE_VLS */
+
 	if (!kvm_arm_vcpu_sve_finalized(vcpu))
 		return 1; /* KVM_REG_ARM64_SVE_VLS */
 
@@ -624,6 +649,10 @@ static int copy_sve_reg_indices(const struct kvm_vcpu *vcpu,
 	if (put_user(reg, uindices++))
 		return -EFAULT;
 	++num_regs;
+
+	/* For Realms only support SVE_VLS */
+	if (kvm_vm_is_realm(vcpu->kvm))
+		return num_regs;
 
 	if (!kvm_arm_vcpu_sve_finalized(vcpu))
 		return num_regs;
@@ -705,6 +734,11 @@ int kvm_arm_get_reg(struct kvm_vcpu *vcpu, const struct kvm_one_reg *reg)
 	if ((reg->id & ~KVM_REG_SIZE_MASK) >> 32 != KVM_REG_ARM64 >> 32)
 		return -EINVAL;
 
+	/*
+	 * We don't filter out the register reads for Realms, like we do for
+	 * the user writes. We expose junk data for the VMM instead of
+	 * denying the requests.
+	 */
 	switch (reg->id & KVM_REG_ARM_COPROC_MASK) {
 	case KVM_REG_ARM_CORE:	return get_core_reg(vcpu, reg);
 	case KVM_REG_ARM_FW:
@@ -716,10 +750,39 @@ int kvm_arm_get_reg(struct kvm_vcpu *vcpu, const struct kvm_one_reg *reg)
 	return kvm_arm_sys_reg_get_reg(vcpu, reg);
 }
 
+#define KVM_REG_ARM_ID_AA64DFR0_EL1	ARM64_SYS_REG(3, 0, 0, 5, 0)
+/*
+ * The RMI ABI only enables setting some GPRs and PC. The selection of GPRs
+ * that are available depends on the Realm state and the reason for the last
+ * exit.  All other registers are reset to architectural or otherwise defined
+ * reset values by the RMM, except for a few configuration fields that
+ * correspond to Realm parameters.
+ */
+static bool validate_realm_set_reg(struct kvm_vcpu *vcpu,
+				   const struct kvm_one_reg *reg)
+{
+	if ((reg->id & KVM_REG_ARM_COPROC_MASK) == KVM_REG_ARM_CORE) {
+		u64 off = core_reg_offset_from_id(reg->id);
+
+		return kvm_realm_validate_core_reg(off);
+	}
+
+	switch (reg->id) {
+	case KVM_REG_ARM_ID_AA64DFR0_EL1:
+	case KVM_REG_ARM64_SVE_VLS:
+		return true;
+	}
+
+	return false;
+}
+
 int kvm_arm_set_reg(struct kvm_vcpu *vcpu, const struct kvm_one_reg *reg)
 {
 	/* We currently use nothing arch-specific in upper 32 bits */
 	if ((reg->id & ~KVM_REG_SIZE_MASK) >> 32 != KVM_REG_ARM64 >> 32)
+		return -EINVAL;
+
+	if (kvm_vm_is_realm(vcpu->kvm) && !validate_realm_set_reg(vcpu, reg))
 		return -EINVAL;
 
 	switch (reg->id & KVM_REG_ARM_COPROC_MASK) {
