d60bea1b1846c40e818c8de54a58187db21833ec
Gavin Shan <gshan@redhat.com>
2024-10-28 09:57:16 +1000

    drivers/mpam: Enable memory bandwidth hard limit mode
    Bit#31 is used to enable hard or soft limit mode for memory bandwidth
    limit in register MPAMCFG_MBW_MAX (offset: 0x208). By default, the
    soft limit mode is enabled.
    
    Enable hard limit mode by setting Bit#31 when the register is updated
    with whatever user configured.
    
    Signed-off-by: Gavin Shan <gshan@redhat.com>

--- drivers/platform/arm64/mpam/mpam_devices.c
+++ drivers/platform/arm64/mpam/mpam_devices.c
@@ -1559,7 +1559,8 @@ static void mpam_reprogram_ris_partid(struct mpam_msc_ris *ris, u16 partid,
 
 	if (mpam_has_feature(mpam_feat_mbw_max, rprops)) {
 		if (mpam_has_feature(mpam_feat_mbw_max, cfg))
-			mpam_write_partsel_reg(msc, MBW_MAX, cfg->mbw_max);
+			mpam_write_partsel_reg(msc, MBW_MAX,
+					       MPAMCFG_MBW_MAX_HARDLIM | cfg->mbw_max);
 		else
 			mpam_write_partsel_reg(msc, MBW_MAX, bwa_fract);
 	}
