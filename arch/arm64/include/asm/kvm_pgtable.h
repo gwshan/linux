99114e2626db36ff3eacdf561b8dfe0348ecb4e0
Steven Price <steven.price@arm.com>
2026-01-21 11:39:34 +0000

    KVM: arm64: Avoid including linux/kvm_host.h in kvm_pgtable.h
    To avoid future include cycles, drop the linux/kvm_host.h include in
    kvm_pgtable.h and include the lightweight headers required for the types
    and inline helpers used there. Additionally provide a forward
    declaration for struct kvm_s2_mmu as it's only used as a pointer in this
    file.
    
    Both pgtable.c and kvm_pkvm.h relied on the indirect inclusion of
    kvm_host.h, so make that explicit.
    
    Reviewed-by: Fuad Tabba <fuad.tabba@linux.dev>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

--- arch/arm64/include/asm/kvm_pgtable.h
+++ arch/arm64/include/asm/kvm_pgtable.h
@@ -7,9 +7,17 @@
 #ifndef __ARM64_KVM_PGTABLE_H__
 #define __ARM64_KVM_PGTABLE_H__
 
+#include <linux/bitfield.h>
 #include <linux/bits.h>
-#include <linux/kvm_host.h>
+#include <linux/kvm_types.h>
+#include <linux/rbtree_types.h>
+#include <linux/rcupdate.h>
 #include <linux/types.h>
+
+#include <asm/memory.h>
+#include <asm/page.h>
+
+struct kvm_s2_mmu;
 
 #define KVM_PGTABLE_FIRST_LEVEL		-1
 #define KVM_PGTABLE_LAST_LEVEL		3
