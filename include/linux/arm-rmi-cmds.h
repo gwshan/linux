6639353a2b7d211f3dc4d6bb69eb61211ca4e287
Steven Price <steven.price@arm.com>
2026-09-12 09:36:05 +0100

    firmware: arm_rmm: Check for RMI support at init
    Query the RMI version number and check if it is a compatible version.
    The first two feature registers are read and exposed for future code to
    use.
    
    We only support this for Little Endian kernels, the Big Endian kernel
    support is anyway marked BROKEN and is being removed.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

---
+++ include/linux/arm-rmi-cmds.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2026 ARM Ltd.
+ */
+
+#ifndef __LINUX_ARM_RMI_CMDS_H_
+#define __LINUX_ARM_RMI_CMDS_H_
+
+#include <linux/arm-smccc-rmi.h>
+#include <linux/bug.h>
+#include <linux/processor.h>
+#include <linux/types.h>
+
+
+/*
+ * rmi_smccc_invoke: Invoke the RMI call and return the results in @regs_out
+ * @regs_in: Registers with the arguments filled in.
+ * @regs_out: Ouptput results from the call.
+ */
+static inline void rmi_smccc_invoke(struct arm_smccc_1_2_regs *regs)
+{
+	struct arm_smccc_1_2_regs args = *regs;
+	unsigned long status;
+
+	while (1) {
+		arm_smccc_1_2_invoke(&args, regs);
+		status = RMI_RETURN_STATUS(regs->a0);
+		if (status != RMI_BUSY && status != RMI_BLOCKED)
+			break;
+		cpu_relax();
+	}
+}
+
+unsigned long rmi_feat_reg(unsigned long index);
+
+#endif
