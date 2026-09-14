be3bf8dfdde05c5520099ecb80aca141341e318a
Suzuki K Poulose <suzuki.poulose@arm.com>
2026-09-14 09:11:21 +0100

    firmware: arm_rmm: Configure the RMM with the host's page size
    RMM v2.0 brings the ability to set the RMM's granule size. Check the
    feature registers and configure the RMM so that it matches the host's
    page size. This means that operations can be done with a granularity
    equal to PAGE_SIZE.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    ---
     Changes since v18:
      * Move the rmi_config_set closer to rmi_configure, drop the comment
     Changes since v17:
      * Move rmi_config_set() out of the header file.
      * Print the error message for rmi_config_set if it fails
     Changes since v15:
      * Actually check the feature register for the host's page-size support.
     Changes since v14:
      * Move the implementation into drivers/firmware/arm_rmm.
     Changes since v13:
      * Moved out of KVM.

--- drivers/firmware/arm_rmm/rmi.c
+++ drivers/firmware/arm_rmm/rmi.c
@@ -84,6 +84,72 @@ unsigned long rmi_feat_reg(unsigned long index)
 }
 EXPORT_SYMBOL_GPL(rmi_feat_reg);
 
+static int rmi_rmm_config_set(unsigned long cfg_ptr)
+{
+	struct arm_smccc_1_2_regs regs = {
+		SMC_RMI_RMM_CONFIG_SET, cfg_ptr,
+	};
+
+	rmi_smccc_invoke(&regs);
+
+	return regs.a0;
+}
+
+static int rmi_configure(void)
+{
+	unsigned long granule_feature;
+	unsigned long granule_size;
+	int ret = 0;
+	struct rmm_config *config;
+
+	switch (PAGE_SIZE) {
+	case SZ_4K:
+		granule_size = RMI_GRANULE_SIZE_4KB;
+		granule_feature = RMI_FEATURE_REGISTER_1_RMI_GRAN_SZ_4KB;
+		break;
+	case SZ_16K:
+		granule_size = RMI_GRANULE_SIZE_16KB;
+		granule_feature = RMI_FEATURE_REGISTER_1_RMI_GRAN_SZ_16KB;
+		break;
+	case SZ_64K:
+		granule_size = RMI_GRANULE_SIZE_64KB;
+		granule_feature = RMI_FEATURE_REGISTER_1_RMI_GRAN_SZ_64KB;
+		break;
+	default:
+		BUILD_BUG();
+	}
+
+	if (!(rmi_feat_reg(1) & granule_feature)) {
+		pr_err("RMM does not support %luKB granules\n",
+		       PAGE_SIZE >> 10);
+		return -ENXIO;
+	}
+
+	config = (struct rmm_config *)get_zeroed_page(GFP_KERNEL);
+	if (!config) {
+		pr_err("Unable to allocate memory for RMM config\n");
+		return -ENOMEM;
+	}
+
+	config->rmi_granule_size = granule_size;
+
+	/*
+	 * For now we set the tracking_region_size to 0 which is the only option
+	 * for 4KB PAGE_SIZE (1GB for 4KB PAGE_SIZE, 32MB/512MB for 16KB/64KB).
+	 * TODO: Support other tracking sizes via Kconfig option for other
+	 * PAGE_SIZES
+	 */
+	config->tracking_region_size = 0;
+
+	ret = rmi_rmm_config_set(virt_to_phys(config));
+	if (ret) {
+		pr_err("RMM config set failed (%d)\n", ret);
+		ret = -EINVAL;
+	}
+
+	free_page((unsigned long)config);
+	return ret;
+}
 
 static int __init arm64_init_rmi(void)
 {
@@ -95,6 +161,10 @@ static int __init arm64_init_rmi(void)
 		return ret;
 
 	ret = rmi_read_features();
+	if (ret)
+		return ret;
+
+	ret = rmi_configure();
 	if (ret)
 		return ret;
 
