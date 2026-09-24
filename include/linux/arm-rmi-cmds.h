f5fb539ab7eacbd47ca079071aa504e5632faad1
Steven Price <steven.price@arm.com>
2026-09-24 14:51:58 +0100

    firmware: arm_rmm: Add support for SRO
    RMM v2.0 introduces the concept of "Stateful RMI Operations" (SRO). This
    means that an SMC can return with an operation still in progress. The
    host is expected to continue the operation until it reaches a conclusion
    (either success or failure). During this process the RMM can request
    additional memory ('donate') or hand memory back to the host
    ('reclaim'). The host can request an in progress operation is cancelled,
    but still continue the operation until it has completed (otherwise the
    incomplete operation may cause future RMM operations to fail).
    
    The SRO is tracked using a struct rmi_sro_state object which keeps track
    of any memory which has been allocated but not yet consumed by the RMM
    or reclaimed from the RMM. This allows the memory to be reused in a
    future request within the same operation. It will also permit an
    operation to be done in a context where memory allocation may be
    difficult (e.g. atomic context) with the option to abort the operation
    and retry the memory allocation outside of the atomic context. The
    memory stored in the struct rmi_sro_state object can then be reused on
    the subsequent attempt.
    
    Wrappers for SRO RMI commands are also provided here because they depend
    on the rmi_sro_execute() implementation added by this patch.
    Delegate/undelegate handles are also added here because they now use the
    SRO/stateful command infrastructure and are also used for the memory
    DONATE/RECLAIM flows.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Co-developed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

--- include/linux/arm-rmi-cmds.h
+++ include/linux/arm-rmi-cmds.h
@@ -8,8 +8,18 @@
 
 #include <linux/arm-smccc-rmi.h>
 #include <linux/bug.h>
+#include <linux/gfp.h>
 #include <linux/processor.h>
+#include <linux/string.h>
 #include <linux/types.h>
+
+#define RMI_MAX_ADDR_LIST	256
+
+struct rmi_sro_state {
+	struct arm_smccc_1_2_regs regs;
+	int addr_count;
+	unsigned long addr_list[RMI_MAX_ADDR_LIST];
+};
 
 #define RMM_BLOCKED_RETRY_COUNT		2
 /*
@@ -44,5 +54,36 @@ static inline void rmi_smccc_invoke(struct arm_smccc_1_2_regs *regs)
 }
 
 unsigned long rmi_feat_reg(unsigned long index);
+
+int rmi_delegate_range(phys_addr_t phys, unsigned long size,
+		       phys_addr_t *out_phys);
+int rmi_undelegate_range(phys_addr_t phys, unsigned long size);
+int rmi_free_delegated_page(phys_addr_t phys);
+
+static inline int rmi_delegate_page(phys_addr_t phys)
+{
+	return rmi_delegate_range(phys, PAGE_SIZE, NULL);
+}
+
+static inline int rmi_undelegate_page(phys_addr_t phys)
+{
+	return rmi_undelegate_range(phys, PAGE_SIZE);
+}
+
+long rmi_sro_memxfer_execute(struct rmi_sro_state *sro, gfp_t gfp);
+void rmi_sro_free(struct rmi_sro_state *sro);
+long rmi_sro_execute(struct arm_smccc_1_2_regs *regs);
+
+/*
+ * Resetting the addr_count is sufficient to ignore the addr_list contents.
+ */
+#define rmi_sro_memxfer_cmd(sro, gfp, ...) ({				\
+	struct rmi_sro_state *__sro = (sro);				\
+	__sro->addr_count = 0;						\
+	__sro->regs = (struct arm_smccc_1_2_regs){ __VA_ARGS__ };	\
+	long __ret = rmi_sro_memxfer_execute(__sro, gfp);		\
+	rmi_sro_free(__sro);						\
+	__ret;								\
+})
 
 #endif
